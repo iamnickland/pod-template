cde45cc2353cde13af184574343062256ee770d7
nick.land <nick.rango.land@gmail.com>
2023-12-03 19:41:51 +0800

    [Update]修改目录名

--- NAME.podspec
+++ NAME.podspec
@@ -36,7 +36,7 @@ TODO: Add long description of the pod here.
   #   '${POD_NAME}' => ['${POD_NAME}/Assets/*.png']
   # }
 
-  # s.public_header_files = 'Pod/Classes/**/*.h'
+  # s.public_header_files = 'Sources/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
--- setup/ConfigureMacOSSwift.rb
+++ setup/ConfigureMacOSSwift.rb
@@ -12,18 +12,9 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
-
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
-      case framework
-        when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 2.2.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 10.0.0"
-          configurator.set_test_framework "quick", "swift", "macos-swift"
-
-        when :none
-          configurator.set_test_framework "xctest", "swift", "macos-swift"
-      end
+      keep_demo = :yes
+      framework = :none
+      configurator.set_test_framework "xctest", "swift", "macos-swift"
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
@@ -37,7 +28,7 @@ module Pod
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
+      `touch Sources/Classes/ReplaceMe.swift`
 
       `mv ./NAME-osx.podspec ./NAME.podspec`
     end
